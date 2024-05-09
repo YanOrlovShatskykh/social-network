e89a1a1050a120282780bd0abbe13fca802e1ec9
Yan Orlov-Shatskykh <yan@MacBook-Pro.local>
2024-05-10 00:12:09 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1-580x435.png" alt='wef' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg' alt='weddf' />
+      </div>
+      <div>
+        ava + descr
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
